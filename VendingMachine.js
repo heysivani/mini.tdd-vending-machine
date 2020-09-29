d7f6f566f791a1826ed97f4e32d2ae9ffc72569a
heysivani <heysivani@gmail.com>
2020-09-29 17:09:03 +0900

    Wrote insertCoin and pressButton tests and code to pass some tests
    Co-authored-by: dius00 <dius00@users.noreply.github.com>
    Co-authored-by: heysivani <heysivani@users.noreply.github.com>

--- VendingMachine.js
+++ VendingMachine.js
@@ -1,4 +1,59 @@
 // your class here
+class VendingMachine {
+  constructor() {
+    this.till = {
+      10: 0,
+      50: 0,
+      100: 0,
+      500: 0,
+    };
+    this.balance = 0;
+    this.row;
+    this.column;
+    this.juice1 = { name: `Apple Juice`, price: 350, count: 5 };
+    this.juice2 = { name: `Apple Juice`, price: 350, count: 5 };
+    this.juice3 = { name: `Apple Juice`, price: 350, count: 5 };
+    this.juice4 = { name: `Apple Juice`, price: 350, count: 5 };
+    this.coffee1 = { name: `Coffe1`, price: 350, count: 5 };
+    this.coffee2 = { name: `Coffe1`, price: 350, count: 5 };
+    this.coffee3 = { name: `Coffe1`, price: 350, count: 5 };
+    this.coffee4 = { name: `Coffe1`, price: 350, count: 5 };
+    this.tea1 = { name: `Tea`, price: 350, count: 5 };
+    this.tea2 = { name: `Tea`, price: 350, count: 5 };
+    this.tea3 = { name: `Tea`, price: 350, count: 5 };
+    this.tea4 = { name: `Tea`, price: 350, count: 5 };
+    this.water1 = { name: `Apple Juice`, price: 350, count: 5 };
+    this.water2 = { name: `Apple Juice`, price: 350, count: 5 };
+    this.water3 = { name: `Apple Juice`, price: 350, count: 5 };
+    this.water4 = { name: `Apple Juice`, price: 350, count: 5 };
+
+    this.inventory = [
+      [this.coffee1, this.coffee2, this.coffee3, this.coffee4],
+      [this.juice1, this.juice2, this.juice3, this.juice4],
+      [this.tea1, this.tea2, this.tea3, this.tea4],
+      [this.water1, this.water2, this.water3, this.water4],
+    ];
+  }
+  insertCoin(denomination) {
+    this.balance += denomination;
+    return this.balance;
+  }
+  pressButton(input) {
+    if (typeof input === "string") {
+      this.row = input;
+      console.log(input);
+    } else {
+      this.column = input;
+      console.log(`${this.row}${this.column}`);
+      const rowChooser = { A: 0, B: 1, C: 2, D: 3 };
+      console.log(this.inventory[rowChooser[this.row]][this.column]);
+      console.log(rowChooser[this.row], this.column);
+      this.inventory[rowChooser[this.row]][this.column].count--;
+    }
+  }
+}
+
+module.exports = VendingMachine;
 /*
   >>> Don't forget to use module.exports!
   What is that? Well, glad you asked.
--- test/test.js
+++ test/test.js
@@ -2,20 +2,59 @@ const VendingMachine = require("../VendingMachine");
 const { expect } = require("chai");
 
 describe("vending machine", () => {
-  it("should accept valid coins", () => {
-    // Setup
-    const machine = new VendingMachine();
+  describe("insertCoin ", () => {
+    it("should accept valid coins and update the till", () => {
+      // Setup
+      const machine = new VendingMachine();
 
-    // Exercise
-    machine.insertCoin(500);
+      // Exercise
+      machine.insertCoin(500);
 
-    // Assert
-    expect(machine.till).to.deep.equal({
-      10: 0,
-      50: 0,
-      100: 0,
-      500: 1,
+      // Assert
+      expect(machine.till).to.deep.equal({
+        10: 0,
+        50: 0,
+        100: 0,
+        500: 1,
+      });
+      expect(machine.balance).to.equal(500); // Use an ES6 getter
     });
-    expect(machine.balance).to.equal(500); // Use an ES6 getter
+    it("should have method insertCoin", () => {
+      const machine = new VendingMachine();
+      expect(typeof machine.insertCoin).to.equal("function");
+    });
+    it("should raise the balance when coin is inserted", () => {
+      const machine = new VendingMachine();
+      machine.insertCoin(100);
+      expect(machine.balance).to.equal(100);
+    });
+  });
+  describe("pressButton", () => {
+    it("should have method pressButton", () => {
+      const machine = new VendingMachine();
+      expect(typeof machine.pressButton).to.equal("function");
+    });
+    it("should save and print the row letter", () => {
+      const machine = new VendingMachine();
+      machine.pressButton("A");
+      expect(machine.row).to.equal("A");
+    });
+    it("should save and print the column", () => {
+      const machine = new VendingMachine();
+      machine.pressButton(1);
+      expect(machine.column).to.equal(1);
+    });
+    it("should decrease item inventory by 1", () => {
+      const machine = new VendingMachine();
+      machine.pressButton("A");
+      machine.pressButton(1);
+      expect(machine.coffee1.count).to.equal(4);
+    });
+  });
+  describe("inventory", () => {
+    it("should have property inventory", () => {
+      const machine = new VendingMachine();
+      expect(machine.inventory).to.not.be.undefined;
+    });
   });
 });
